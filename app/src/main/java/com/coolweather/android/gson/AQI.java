58c79326b1c21cf027341f50c3c5daf77f049c80
Ken <494547652@qq.com>
2018-03-24 02:00:29 +0800

    编写完成天气界面

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Ken on 2018/3/24.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
